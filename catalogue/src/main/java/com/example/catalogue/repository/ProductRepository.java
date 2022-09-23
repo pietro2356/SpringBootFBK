d94c771bf59952bb79fb94da1ac370d5572da43f
pietro2356 <pietro.rocchio@gmail.com>
2022-09-23 11:13:02 +0200

    Implementato MongoDB

--- catalogue/src/main/java/com/example/catalogue/repository/ProductRepository.java
+++ catalogue/src/main/java/com/example/catalogue/repository/ProductRepository.java
@@ -3,9 +3,12 @@ package com.example.catalogue.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import com.example.catalogue.domain.Product;
 
+
+@EnableMongoRepositories(basePackages = "com.example.catalogue.configuration")
 public interface ProductRepository extends MongoRepository<Product, String> {
 
 	List<Product> findByCategory(String category);
