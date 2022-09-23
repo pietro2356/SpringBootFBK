c7f8ed936d120568a170f3e88e5d97a2708f4cf0
pietro2356 <pietro.rocchio@gmail.com>
2022-09-23 18:21:54 +0200

    Edit application

--- catalogue/src/main/java/com/example/catalogue/rest/ProductController.java
+++ catalogue/src/main/java/com/example/catalogue/rest/ProductController.java
@@ -9,37 +9,39 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.catalogue.domain.Product;
 import com.example.catalogue.service.ProductService;
 
 @RestController
+@RequestMapping("/api")
 public class ProductController {
 
 	@Autowired
 	private ProductService service;
 	
-	@GetMapping("/api/products")
+	@GetMapping("/products")
 	public List<Product> getProducts() {
 		return service.getProducts();
 	}
 	
-	@GetMapping("/api/products/{id}")
+	@GetMapping("/products/{id}")
 	public Product getProduct(@PathVariable String id) {
 		return service.getProduct(id);
 	}
 
-	@GetMapping("/api/products/category/{category}")
+	@GetMapping("/products/category/{category}")
 	public List<Product> getProductsByCategory(@PathVariable String category) {
 		return service.findByCategory(category);
 	} 
 	
-	@PostMapping("/api/products")
+	@PostMapping("/products")
 	public Product createProduct(@RequestBody Product product) {
 		return service.createProduct(product);
 	}
-	@PutMapping("/api/products/{id}/availability/{diff}")
+	@PutMapping("/products/{id}/availability/{diff}")
 	public Product changeAvailability(@PathVariable String id, @PathVariable Integer diff) {
 		return service.updateAvailability(id, diff);
 	}
