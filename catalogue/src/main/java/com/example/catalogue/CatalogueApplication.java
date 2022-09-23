21c5ebc11e03d17de0a029d3c189087aa4a8a2f1
pietro2356 <pietro.rocchio@gmail.com>
2022-09-23 11:43:09 +0200

    EnableAutoConfiguration

--- catalogue/src/main/java/com/example/catalogue/CatalogueApplication.java
+++ catalogue/src/main/java/com/example/catalogue/CatalogueApplication.java
@@ -1,8 +1,12 @@
 package com.example.catalogue;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @SpringBootApplication
 public class CatalogueApplication {
 
