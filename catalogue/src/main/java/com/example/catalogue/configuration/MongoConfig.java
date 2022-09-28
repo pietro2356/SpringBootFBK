b3944e68830d89c57e57d8b400850903667f20d3
pietro2356 <pietro.rocchio@gmail.com>
2022-09-28 11:33:46 +0200

    Aggornate le properties

--- catalogue/src/main/java/com/example/catalogue/configuration/MongoConfig.java
+++ catalogue/src/main/java/com/example/catalogue/configuration/MongoConfig.java
@@ -19,6 +19,7 @@ public class MongoConfig extends AbstractMongoClientConfiguration{
     @Override
     public MongoClient mongoClient() {
         ConnectionString connectionString = new ConnectionString("mongodb://mongo:27017/fbk");
+        // ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/fbk");
         MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
             .applyConnectionString(connectionString)
             .build();
